8e1b5a1cef2fc8ba3c302d06c98798850dfa3e51
Kelviiiin <33090236+Kelviiiin@users.noreply.github.com>
2020-04-27 23:06:11 +0200

    Create Project

---
+++ Assets/Scripts/DisplayPositionSpeed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayPositionSpeed : MonoBehaviour
+{
+    public Transform player;
+    public Text displayText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x_position = player.position.x;
+        float y_position = player.position.y;
+        float z_position = player.position.z;
+
+        float movespeed = 0;
+
+        displayText.text = x_position.ToString("0") + " x " + y_position.ToString("0") + " x " + z_position.ToString("0") + "\n" + movespeed.ToString("0");
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+
+    public float restartDelay = 2f;
+
+    public void GameOver()
+    {
+        if (!gameHasEnded)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
